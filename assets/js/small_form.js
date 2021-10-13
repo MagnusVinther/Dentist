89a77ba5e17763d7ea3c26cdcfa47a28cb60759d
Hydrille <03rytter@gmail.com>
2021-10-13 09:02:12 +0200

    removing incorrect comits

--- assets/js/small_form.js
+++
@@ -1,7 +0,0 @@
-import { createNotificationBanner } from "./notification_banner.js";
-document.querySelector("form").addEventListener("submit", (e) => {
-	e.preventDefault();
-	console.log("test");
-	createNotificationBanner("test");
-	return false;
-});
